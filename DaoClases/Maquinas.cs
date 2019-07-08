bffd4b78dd7bf0f3171d499b6bc2128e2b708408
Felipe Franco <ffrancogalan@gmail.com>
2019-07-08 16:25:04 -0400

    version 1

---
+++ DaoClases/Maquinas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DaoClases
+{
+    public class Maquinas
+    {
+        private int idMaquina;
+        private string NombreMaquina;
+        private string marca;
+        private string color;
+
+        public int IdMaquina
+        {
+            get
+            {
+                return idMaquina;
+            }
+
+            set
+            {
+                idMaquina = value;
+            }
+        }
+
+        public string NombreMaquina1
+        {
+            get
+            {
+                return NombreMaquina;
+            }
+
+            set
+            {
+                NombreMaquina = value;
+            }
+        }
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+
+            set
+            {
+                marca = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+
+            set
+            {
+                color = value;
+            }
+        }
+    }
+}
---
+++ WindowsViews/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsViews
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGestionMaquinas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
